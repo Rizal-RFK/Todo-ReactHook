589fb894095a3932b74f734fe81ef0b882ed618d
Rizal-RFK <r1z4lr4fl1@gmail.com>
2022-06-17 21:45:39 +0700

    todo reacthook pertama

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Fragment } from 'react';
+import TodoForm from './component/TodoForm';
+import TodoList from './component/TodoList';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Todo from './component/CreateTodo';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <Fragment>
+        <Router>
+          <nav class="navbar navbar-expand-sm navbar-light bg-light py-3 px-5">
+            <h2 class="mr-5">ToDoApp</h2>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <a class="nav-item nav-link" href="#"><Link to="/">Home</Link></a>
+                <a class="nav-item nav-link" href="#"><Link to="/createtodo">Create To Do</Link></a>
+              </div>
+            </div>
+          </nav>
+          <Routes>
+          <Route exact path='/' element={<Home />}></Route>
+            <Route exact path='/createtodo' element={< TodoList />}></Route>
+          </Routes>
+      </Router>
+      </Fragment>
+    );
+  }
+      
+function Home() {
+    return (
+      <section class="section mt-5">
+        <div class="container bg-info p-5">
+          <div class="row">
+            <div class="col-lg-12">
+            <div class="jumbotron p-5">
+              <b class="display-2">Hello, World!</b>
+              <p class="lead">Previously, let me introduce my name Rizal Rafli, on this occasion I will demonstrate my simple project from "Cooderu Bootcamp" with the name "ToDoApp" with React-Hook.</p>
+              <hr class="my-3" />
+              <p>If you want to know the app that I made, please click the button below, please leave a comment, and don't hesitate to give feedback.</p>
+              <Link to="/createtodo">
+                <button class="btn btn-outline-primary mt-5" >Get Started</button>
+              </Link>
+            </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
 }
 
 export default App;
+
+{/* <div className="todo-app">
+     <TodoList />
+    </div> */}
---
+++ src/component/TodoForm.js
@@ -0,0 +1,61 @@
+import React, {useState, useEffect, useRef} from 'react'
+
+function TodoForm(props) {
+    const [input, setInput] = useState(props.edit ? props.edit.value:'');
+
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        inputRef.current.focus()
+    });
+
+    const handleChange = e => {
+        setInput(e.target.value);
+        };
+
+    const handleSubmbit = e => {
+        e.preventDefault();
+        props.onSubmit({
+            id: Math.floor(Math.random() *1000),
+            text: input
+        });
+
+        setInput(''); 
+    }
+
+  return (
+    <form className='todo-form' onSubmit={handleSubmbit}>
+        {props.edit ? (
+        <>
+        <input 
+        type='text'
+        placeholder='Update Your Item'
+        value={input}
+        name='text'
+        className='todo-input edit'
+        onChange={handleChange}
+        ref={inputRef}
+        />
+        <button className='todo-button edit'>Update</button> 
+        </>
+        ) :
+        ( 
+        <>
+        <input 
+        type='text'
+        placeholder='add a todo'
+        value={input}
+        name='text'
+        className='todo-input'
+        onChange={handleChange}
+        ref={inputRef}
+        />
+        <button className='todo-button'>Add todo</button>
+        </>
+        )}
+
+    </form>
+  )
+}
+
+export default TodoForm
